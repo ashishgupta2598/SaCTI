5c1d15ef93371d413b829c74d0e39d38bf1f3e6d
ashishgupta2598 <ashishgupta2598@gmail.com>
2022-08-02 12:08:30 +0530

    .

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from json.tool import main
-from unicodedata import name
 import torch
 from sklearn.metrics import classification_report
 from data_config import get_path
 from tpipeline import TPipeline,TaggerDataset
 import time
 import argparse
-print("hello")
+
 def acc(path,test_d_path):
     f = open(test_d_path,'r')
     gold =  f.readlines()
@@ -42,7 +40,7 @@ def acc(path,test_d_path):
     f.write(str(classification_report(preds, targs, target_names=target_names)))
     f.close()
 
-def run(panelty,model_path,train_path,dev_path,test_d_path):
+def run(panelty,model_path,train_path,dev_path,test_d_path,epochs,btch_size):
     torch.cuda.empty_cache()
     trainer = TPipeline(
             training_config={
@@ -51,10 +49,11 @@ def run(panelty,model_path,train_path,dev_path,test_d_path):
             'save_dir': model_path, # directory for saving trained model
             'train_conllu_fpath': train_path, # annotations file in CONLLU format  for training
             'dev_conllu_fpath': dev_path, # annotations file in CONLLU format for development
-            'max_epoch': 1,
-            "batch_size":30,
+            'max_epoch': epochs,
+            "batch_size":btch_size,
             'panelty':panelty
         })
+
     trainer.train()
     test_set = TaggerDataset(
         config=trainer._config,
@@ -66,25 +65,27 @@ def run(panelty,model_path,train_path,dev_path,test_d_path):
     test_batch_num = len(test_set) // trainer._config.batch_size + (len(test_set) % trainer._config.batch_size != 0)
     result,path = trainer._eval_posdep(data_set=test_set, batch_num=test_batch_num,
                             name='testfaL', epoch=-1)
-    print(path)
+    print("Path of test preds ",path)
     del trainer
     torch.cuda.empty_cache()
     acc(path,test_d_path)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, default='.', help='Model path')
+    parser.add_argument('--model_path', type=str, default='./model1', help='Model path')
     parser.add_argument('--experiment', type=str, default='sactii fine', help='Experiment type')
+    parser.add_argument('--epochs', type=int, default=70, help='epochs')
+    parser.add_argument('--batch_size', type=int, default=50, help='batch size')
+
     args = parser.parse_args()
     exp_type = args.experiment
-    print("gn ",exp_type)
 
     train_path,dev_path,test_path = get_path(exp_type)
     model_path = args.model_path #'./models/'
     
     
     panelty = 0.01
-    run(panelty,model_path,train_path,dev_path,test_path)
+    run(panelty,model_path,train_path,dev_path,test_path,args.epochs,args.batch_size)
 
 
 
