203b1f3e154b4a4be4b8646061af1618442fbf51
ashishgupta2598 <ashishgupta2598@gmail.com>
2022-08-02 13:25:47 +0530

    .

--- data_config.py
+++ data_config.py
@@ -4,29 +4,29 @@ from logging import exception
 
 def get_path(exp_type):
     if exp_type=='saCTI-large coarse':
-        train = '../sacti/coarse/train.conll'
-        dev = '../sacti/coarse/dev.conll'
-        test = '../sacti/coarse/test.conll'
+        train = 'data/sacti/coarse/train.conll'
+        dev = 'data/sacti/coarse/dev.conll'
+        test = 'data/sacti/coarse/test.conll'
     elif exp_type=='saCTI-large fine':
-        train = '../sacti/fine/train.conll'
-        dev = '../sacti/fine/dev.conll'
-        test = '../sacti/fine/test.conll'
+        train = 'data/sacti/fine/train.conll'
+        dev = 'data/sacti/fine/dev.conll'
+        test = 'data/sacti/fine/test.conll'
     elif exp_type=='saCTI-base coarse':
-        train = '../coling/coarse/train.conll'
-        dev = '../coling/coarse/dev.conll'
-        test = '../coling/coarse/test.conll'
+        train = 'data/coling/coarse/train.conll'
+        dev = 'data/coling/coarse/dev.conll'
+        test = 'data/coling/coarse/test.conll'
     elif exp_type=='saCTI-base fine':
-        train = '../coling/fine/train.conll'
-        dev = '../coling/fine/dev.conll'
-        test = '../coling/fine/test.conll'
+        train = 'data/coling/fine/train.conll'
+        dev = 'data/coling/fine/dev.conll'
+        test = 'data/coling/fine/test.conll'
     elif exp_type=='marathi':
-        train = '../marathi/coarse/train.conll'
-        dev = '../marathi/coarse/dev.conll'
-        test = '../marathi/coarse/test.conll'
+        train = 'data/marathi/coarse/train.conll'
+        dev = 'data/marathi/coarse/dev.conll'
+        test = 'data/marathi/coarse/test.conll'
     elif exp_type=='english':
-        train = '../english/coarse/train.conll'
-        dev = '../english/coarse/dev.conll'
-        test = '../english/coarse/test.conll'
+        train = 'data/english/coarse/train.conll'
+        dev = 'data/english/coarse/dev.conll'
+        test = 'data/english/coarse/test.conll'
     else:
         raise Exception("Please select a proper experimnet from the list")
 
